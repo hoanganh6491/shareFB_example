fe3d137341b5f3ceb9e2eb91a2892b126f0320d9
haipt <haipt@milo.local>
2015-06-26 09:09:20 +0700

    add project

---
+++ sharefb_example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sharefb_example
+//
+//  Created by haipt on 6/26/15.
+//  Copyright (c) 2015 haipt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+
+
+}
+
